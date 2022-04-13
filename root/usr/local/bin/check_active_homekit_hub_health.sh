605b2c2936870e9d2355345eb3dd343fb41e1509
dzhelev <dzhelev@vmware.com>
2022-04-13 10:46:00 +0300

    Fix check_active_homekit_hub_health.sh

--- root/usr/local/bin/check_active_homekit_hub_health.sh
+++ root/usr/local/bin/check_active_homekit_hub_health.sh
@@ -3,8 +3,21 @@
 
 source /etc/environment
 
-TIME_AGO=$1
+TIME_AGO="${1:-10min}"
 DATE=$(date --iso-8601=seconds -d "$TIME_AGO ago")
 
-jq -re --arg date "$DATE" 'select ((.time_iso8601 >= $date))' $LOGFILE
-exit $?
+
+cat $LOGFILE | jq -re --arg date "$DATE" 'select ((.time_iso8601 >= $date))' > /dev/null
+if [[ $? -eq 0 ]]; then
+ LAST_HUB_JSON=$(tail -1 $LOGFILE | jq -rec '({remote_addr, time_iso8601})')
+ ACTIVE_HUB_IP=$(echo $LAST_HUB_JSON | jq -re '.remote_addr')
+ LAST_EXPORTED_STATE=$(echo $LAST_HUB_JSON | jq -re '.time_iso8601')
+ echo "$(date +%Y-%m-%d\ %H:%M:%S) [SUCCESS] HomeKit Active Hub [$ACTIVE_HUB_IP] successfully reported state on [$LAST_EXPORTED_STATE] which is less than [$TIME_AGO] ago"
+ exit 0
+else
+ LAST_HUB_JSON=$(tail -1 $LOGFILE | jq -rec '({remote_addr, time_iso8601})')
+ ACTIVE_HUB_IP=$(echo $LAST_HUB_JSON | jq -re '.remote_addr')
+ LAST_EXPORTED_STATE=$(echo $LAST_HUB_JSON | jq -re '.time_iso8601')
+ echo "$(date +%Y-%m-%d\ %H:%M:%S) [FAILED] HomeKit Active Hub [$ACTIVE_HUB_IP] last reported state on [$LAST_EXPORTED_STATE] which is more than [$TIME_AGO] ago"
+ exit 1
+fi
--- root/usr/local/bin/monit2telegram.sh
+++ root/usr/local/bin/monit2telegram.sh
@@ -2,4 +2,4 @@
 #
 # Monit EXEC handler that sends monit notifications via Telegram
 #
-/usr/local/bin/sendtelegram -c /etc/telegramrc -m "Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
+/usr/local/bin/sendtelegram.sh -c /etc/telegramrc -m "Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
